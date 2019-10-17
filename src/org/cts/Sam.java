df24aa85ee9fd5605214a931d1306f7caaa8f278
Shankar0302 <sankar03m@gmail.com>
2019-10-17 09:01:21 +0530

    first commit

---
+++ src/org/cts/Sam.java
@@ -0,0 +1,35 @@
+package org.cts;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Sam {
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sathish Kumar\\eclipse-workspace\\shalini\\Sample\\driv\\chromedriver.exe");
+		WebDriver d = new ChromeDriver();
+		d.get("http://demo.automationtesting.in/Alerts.html");
+		WebElement h = d.findElement(By.xpath("//button[@class='btn btn-danger']"));
+		h.click();
+		Alert a =  d.switchTo().alert();
+		a.accept();
+	    
+	   
+		
+ 		
+
+	
+		
+	}
+
+}
